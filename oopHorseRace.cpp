3b46c5a2dbc5f86390beff8a53ee7bd1c37376e2
Humberto Torres <133621997+H05T3@users.noreply.github.com>
2025-02-05 18:56:11 +0000

    fixed bugs

--- oopHorseRace.cpp
+++ oopHorseRace.cpp
@@ -20,24 +20,25 @@ class Horse{
 			Horse::trackLength = trackLength;
 		}
 		void advance(){
-			srand(time(NULL));
 			int coin = rand() % 2;
 			Horse::position += coin;
 		}
 		void printLane(){
-			for(int i = 0; i<Horse::trackLength; i++){
+			for(int i = 0; i<trackLength; i++){
 				if(i != position){
 					cout<<". ";
 				}else{
 					cout<<i<<" ";
+			
+				}
 			}
 			cout<<endl;
 		}
 		bool isWinner(){
 			bool winner = false;
-			if(Horse::position >= Horse::trackLength-1){
+			if(position >= trackLength-1){
 				winner = true;
-				cout << "Horse " << Horse::horseID << " won the race!!" << endl;
+				cout << "Horse " << horseID << " won the race!!" << endl;
 			}
 			return winner;
 		}
@@ -48,7 +49,7 @@ class Race{
 	private:
 		const int trackLength = 10;
 		const int numHorses = 5;
-		Horse horses[numHorses];
+		Horse* horses[numHorses];
 	public:
 		Race(){ 
 			for(int i = 0;i<numHorses;i++){
@@ -59,9 +60,9 @@ class Race{
 			bool KEEPGOING = true;
 			while(KEEPGOING){
 				for(int i = 0; i<numHorses; i++){
-					Race::horses[i].advance();
-					Race::horses[i].printLane();
-					if(isWinner(i, horses)){
+					horses[i].advance();
+					horses[i].printLane();
+					if(horses[i].isWinner(){
 						KEEPGOING = false;
 					}
 				}
@@ -79,7 +80,8 @@ class Race{
 };
 
 int main(){
+	srand(time(NULL));
 	Race r;
-	Race.run();
+	r.run();
 	return 0;
 }
