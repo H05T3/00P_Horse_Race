32ca73cf2d33e622e7a4f5e7f25fcb4a05b4b3a6
Humberto Torres <133621997+H05T3@users.noreply.github.com>
2025-02-05 18:40:47 +0000

    coded my plan

--- oopHorseRace.cpp
+++ oopHorseRace.cpp
@@ -2,32 +2,84 @@
 #include<ctime>
 #include <iostream>
 
+using namespace std;
+
 class Horse{
 	private:
-		int horseNum;
-		int position;
-		bool isWinner;
+		int horseID;
+		int position = 0;
+		int trackLength;
 	public:
 		Horse(){
-			horseNum = -1;
+			horseID = 0;
 			position = 0;
-			isWinner = false;
+			trackLength = 0;
 		}
-		Horse(int horseNumm){
-			Horse::horseNum = horseNum;
+		Horse(int horseID, int trackLength){
+			Horse::horseID = horseID;
+			Horse::trackLength = trackLength;
 		}
 		void advance(){
 			srand(time(NULL));
 			int coin = rand() % 2;
 			Horse::position += coin;
 		}
+		void printLane(){
+			for(int i = 0; i<Horse::trackLength; i++){
+				if(i != position){
+					cout<<". ";
+				}else{
+					cout<<i<<" ";
+			}
+			cout<<endl;
+		}
+		bool isWinner(){
+			bool winner = false;
+			if(Horse::position >= Horse::trackLength-1){
+				winner = true;
+				cout << "Horse " << Horse::horseID << " won the race!!" << endl;
+			}
+			return winner;
+		}
 
 };
 
-void advance(int horseNum, int* horses);
-void printLane(int horseNum, int* horses);
-bool isWInner(int horseNum, int* horses);
-const int RACELEN = 10;
+class Race{
+	private:
+		const int trackLength = 10;
+		const int numHorses = 5;
+		Horse horses[numHorses];
+	public:
+		Race(){ 
+			for(int i = 0;i<numHorses;i++){
+				Race::horses[i] = Horse(i,trackLength);
+			}
+		}
+		void run(){
+			bool KEEPGOING = true;
+			while(KEEPGOING){
+				for(int i = 0; i<numHorses; i++){
+					Race::horses[i].advance();
+					Race::horses[i].printLane();
+					if(isWinner(i, horses)){
+						KEEPGOING = false;
+					}
+				}
+				if(KEEPGOING){
+					string usInput;
+					cout<<"Press q to quit or enter to continue:";
+					getline(cin, usInput);
+
+					if(usInput == "q"){
+						KEEPGOING = false;
+					}
+				}
+			}
+		}
+};
 
 int main(){
+	Race r;
+	Race.run();
+	return 0;
 }
